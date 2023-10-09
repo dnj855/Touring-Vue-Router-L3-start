d8d1548e009599679323d19d1cf20a6e1ed0e8d4
Cédric Lang-Roth <contact@clangroth.fr>
2023-10-09 14:44:39 +0200

    Little change

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "https://my-json-server.typicode.com/Code-Pop/Touring-Vue-Router",
+  baseURL: "https://my-json-server.typicode.com/dnj855/Touring-Vue-Router-L3-start",
   withCredentials: false,
   headers: {
     Accept: "application/json",
